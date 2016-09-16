d7204e8bf26ce97f9c55d71ade4374a1d62018a8
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-09-16 10:41:38 -0700

    FIXME. Getting weird double array access error.

--- sudoku-challenge-master/sudoku-challenge-master/runner.rb
+++ sudoku-challenge-master/sudoku-challenge-master/runner.rb
@@ -14,10 +14,35 @@ require_relative 'sudoku'
 
 board_string = File.readlines('sudoku_puzzles.txt').first.chomp
 
-solved_board = solve(board_string)
-if solved?(solved_board)
-  puts "The board was solved!"
-  puts pretty_board(solved_board)
-else
-  puts "The board wasn't solved :("
+# solved_board = solve(board_string)
+# if solved?(solved_board)
+#   puts "The board was solved!"
+#   puts pretty_board(solved_board)
+# else
+#   puts "The board wasn't solved :("
+# end
+
+# Generate a nil double array for the sudoku board
+# Based on the read line of string, populate any 
+# numeric values into the board, all other values are
+# left as nil
+
+def generate_grid(board_string)   current_grid = Array.new(3, Array.new(3) {
+["-", "X"].sample })
+
+  parse_char = board_string.split('')
+  parse_count = 0
+
+  test = current_grid.length
+
+  3.times do |row|
+    3.times do |idx|
+      current_grid[row][idx] = parse_char[parse_count]
+      parse_count += 1
+    end
+  end
+
+  return current_grid
 end
+
+p generate_grid(board_string)
